8db904ffa08ed51d37c8895a324f8eddb3fc5063
Alex Lee <arl7816@g.rit.edu>
2023-04-07 13:21:32 -0400

    some small little changeyes

--- Algorithms/calc.py
+++ Algorithms/calc.py
@@ -1,23 +1,77 @@
 import math
 
-def diff(f: function, x: int, h=0.000000001) -> int:
+def diff(f, x: int, h=0.000000001) -> int:
   return (f(x+h)-f(x))/h
 
-
-def integrate(f: function, a: int, b: int, n=10000) -> int:
+def base_integrate(f, a: int, b: int, n: int, k: int) -> float:
   total = 0
   deltaX = (b-a)/n
 
-  index = 1
-
   for _ in range(n):
-    total += f(a+index*deltaX)*deltaX
-    index += 1
+    total += (f(a+k*deltaX)*deltaX)
+    k += 1
   return total
 
-def mean_value(f: function, a: int, b: int) -> int:
+def trapezoid_integration(f, a: int, b: int, n: int) -> float:
+  deltaX = (b-a)/n
+
+  fo = f(a)
+  fn = f(a + n*deltaX)
+
+  total = 0
+  for k in range(1,n):
+    total += (2*f(a + k*deltaX))
+
+  total = (deltaX/2)*(fo + fn + total)
+  return total
+
+def simp_integrate(f, a: int, b: int, n: int) -> float:
+  deltaX = (b-a)/n
+
+  fo = f(a)
+  fn = f(a + n*deltaX)
+
+  total = 0
+  for k in range(1,n):
+    total += (2*(1+(k % 2)) * f(a + k*deltaX))
+  
+  total = (deltaX/3) * (fo+fn+total)
+  return total
+
+def integrate(f, a: int, b: int, method: str, n=100) -> float:
+  total = None
+
+  if method == "left":
+    total = base_integrate(f, a, b, n, 0)
+  elif method == "mid":
+    total = base_integrate(f, a, b, n, 0.5)
+  elif method == "trapezoid":
+    total = trapezoid_integration(f, a, b, n)
+  elif method == "simp":
+    total = simp_integrate(f, a, b, n)
+  else: 
+    # right hand method --> most common
+    total = base_integrate(f, a, b, b, 1)
+  
+  return total
+
+def mean_value(f, a: int, b: int) -> int:
   return (f(b) - f(a)) / (b-a)
 
+
+def ln(x: int, n=100) -> float:
+  def f(x: int) -> float:
+    return 1/x
+
+  return integrate(f, 1, x, "simp", n)
+
+def loga(a: int, b: int, n=100) -> float:
+  lna = ln(a, n)
+
+  def f(x: int) -> float:
+    return 1/(x*lna)
+
+  return integrate(f, 1, b, "simp", n)
 
 class Matrix:
   pass
--- main/tester.py
+++ main/tester.py
@@ -2,10 +2,12 @@
 from time import perf_counter
 from random import randint as ran
 import sys
+from math import e, log
 
 sys.path.append("./SearchesAndSorts")
 sys.path.append("./DataStructures")
 sys.path.append("./DataTypes")
+sys.path.append("./Algorithms")
 
 # custom imports (if an import is not working, make sure to change the
 # json file in .vscode so the code knows where to look)
@@ -17,6 +19,8 @@ from Stack import Stack
 from Queue import Queue
 from HashSet import HashSet
 from DoublyLinkedList import DoublyLinkedList
+from calc import integrate, ln, loga
+from math import e
 
 def random_array(size: int, low:int, high:int) -> list[int]:
   """
@@ -34,12 +38,29 @@ def random_array(size: int, low:int, high:int) -> list[int]:
 
   return [ran(low, high) for _ in range(size)]
 
-def main():
-  mapset = HashSet()
-  
-  mapset.assign(4)
+def f(x):
+  return 3*pow(e, (-x/4))
 
-  print(mapset)
+def main():
+  #mapset = HashSet()
+  
+  #mapset.assign(4)
+
+  #print(mapset)
+
+  x = 1
+  a = 2
+
+  n = 20
+  print("Answer should be: 9")
+  print("Right-point method with n =", n, ":", integrate(f, 1, 2, "right", n))
+  print("Left-point method with n =", n, ":", integrate(f, 1, 2, "left", n))
+  print("mid-point method with n =", n, ":", integrate(f, 0, 1, "mid", n))
+  print("trapezoid method with n =", n, ":", integrate(f, 0, 9, "trapezoid", n))
+  print("simp method with n =", n, ":", integrate(f, -3, 3, "simp", n))
+
+  print("ln(" + str(x) + ") is:", log(x,e), " vs estimate: ", ln(x, 50))
+  print("log(" + str(x) + ", " + str(a) + ") is:", log(x,a), " vs estimate: ", loga(a,x, 50))
 
   return None
 
